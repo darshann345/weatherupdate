dc677308ccc964cb1d79c9d61e0b4d2db92ac0b8
Darshan <darshann679596@gmail.com>
2025-01-07 12:18:34 +0530

    commit success

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './Components/Login/Login';
 import Weather from './Components/Weather/Weather';
 function App() {
   return (
     <>
-      {/* <Login /> */}
       <Weather/>
     </>
     
